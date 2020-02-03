a74a334acf8b4ee15ed544c31288983e225b2781
Guillermo Hernandez <guillermo.hernandez@correounivalle.edu.co>
2020-02-02 22:59:41 -0500

    Making conditionals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
-import { HeaderComponent } from './components/header/header.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    UserComponent,
   ],
   imports: [
     BrowserModule
